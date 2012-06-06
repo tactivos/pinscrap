ac422aec50b9a0d2a9911162cb19461fddcf40c8
Pato Jutard <pato@tactivos.com>
2012-06-06 14:38:55 -0300

    Changing to POST

--- parser.rb
+++ parser.rb
@@ -4,14 +4,15 @@ require 'nokogiri'
 require 'open-uri'
 
 # Get a Nokogiri::HTML:Document for the page we’re interested in...
-get "/" do 
-	doc = Nokogiri::HTML(open('http://pinterest.com/patojutard/gadgets-i-want/'))
+post "/" do 
+
+	doc = Nokogiri::HTML(open(params.url))
 
 	# Do funky things with it using Nokogiri::XML::Node methods...
 	# Search for nodes by css
 	content = []
 	doc.css('.PinImage.ImgLink').each do |link|
-	 	content << "http://pinterest.com#{link['href']}|"	
+	 	content << "#{link['href']}|"	
 	end
 
 	content
